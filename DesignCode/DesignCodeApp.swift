29a4f9487238b834827888bbc08286b5abf5feef
zhaslan057 <z.tanerbergenov@gmail.com>
2022-10-11 15:12:49 +0600

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Жаслан Танербергенов on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
